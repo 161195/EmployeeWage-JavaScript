33edb0e1d7491e63faabfe21ab95573213c3e722
“mayuri” <mayurisalunkhe01@gmail.com>
2021-11-29 20:00:07 +0530

    EmployeeWage JavaScript added

--- EmpWage.js
+++ EmpWage.js
@@ -1,13 +1,22 @@
-//UC1
-//check employee present or not
+//UC2
+//check employee is part or full time and Employee daily wage
 //constant variable
-const IS_ABSENT = 0;
-let empCheck = Math.floor(Math.random()*10) % 2;
-if (empCheck == IS_ABSENT)
+const IS_FULL_TIME = 1;
+const IS_PART_TIME = 2;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random()*10) % 3;
+switch (empCheck)
 {
-    console.log("Employee is Absent");
+    case IS_FULL_TIME:
+        empHrs = 8;
+        break;
+    case IS_PART_TIME:
+        empHrs = 4;
+        break;
+    default:
+        empHrs = 0;
 }
-else 
-{
-    console.log("Employee is Present");
-}
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage : " +empWage);
